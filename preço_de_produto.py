ea014b847a42d490ec25c896b93b268826c478a0
Malexjp <malexjp@gmail.com>
2016-04-06 12:55:47 -0300

    Update preço_de_produto.py
    Feito modificação no programa utilizando o comando "for in range" na linha 13.

--- preço_de_produto.py
+++ preço_de_produto.py
@@ -6,18 +6,23 @@
 
 # Programa preço de produtos
 
-preços = [float(input("Digite o preço do produto 1:")), float(input("Digite o preço do produto 2:")), float(input("Digite o preço do produto 3:")), float(input("Digite o preço do produto 4:")), float(input("Digite o preço do produto 5:"))]
+# preços = [float(input("Digite o preço do produto 1:")), float(input("Digite o preço do produto 2:")), float(input("Digite o preço do produto 3:")), float(input("Digite o preço do produto 4:")), float(input("Digite o preço do produto 5:"))]
 
+precos = list()
+
+for p in range (0, 5):
+ precos.append(float(input("Digite o preço do Produto %d: " % (p + 1))))
+ 
 inferior_50 = 0
 entre_50_e_80 = 0
 acima_de_80 = 0
 
-for preço in preços:
-	if preço < 50:
+for preco in precos:
+	if preco < 50:
 		inferior_50 = inferior_50 + 1
-	if preço >= 50 and preço <= 80:
+	if preco >= 50 and preco <= 80:
 		entre_50_e_80 = entre_50_e_80 + 1
-	if preço > 80:
+	if preco > 80:
 		acima_de_80 = acima_de_80 + 1
 
 print("Quantidade de produtos inferior a R$ 50,00 é: %d" % inferior_50)
